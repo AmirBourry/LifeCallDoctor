941ce28fd9ccd5a700b0deea1d72d80fcc7345f3
AmirBourry <95630288+AmirBourry@users.noreply.github.com>
2024-12-10 15:10:43 +0100

    work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {SidebarComponent} from './components/sidebar/sidebar.component';
+import {DoctorComponent} from './components/doctor/doctor.component';
+import {NgIf} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {FlexModule} from '@ngbracket/ngx-layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    SidebarComponent,
+    DoctorComponent,
+    NgIf,
+    RouterOutlet,
+    FlexModule
+  ],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {DoctorComponent} from './components/doctor/doctor.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'doctor', component: DoctorComponent},
+];
