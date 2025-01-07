7c736b171a254a25a9e0225689b3dc48461b8bbb
AmirBourry <95630288+AmirBourry@users.noreply.github.com>
2025-01-07 14:36:22 +0100

    implements doctor page

--- src/app/components/doctor/following/following.component.ts
+++ src/app/components/doctor/following/following.component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
+import {MatFormField, MatFormFieldModule, MatLabel} from '@angular/material/form-field';
+import {MatOption, MatSelect} from '@angular/material/select';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardModule,
+  MatCardTitle
+} from '@angular/material/card';
+import {CommonModule, NgClass} from '@angular/common';
+import {MatDivider} from '@angular/material/divider';
+import {FlexModule} from '@ngbracket/ngx-layout';
+import {MatChipsModule} from '@angular/material/chips';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-following',
-  imports: [],
+  imports: [
+    MatFormField,
+    MatLabel,
+    MatSelect,
+    MatOption,
+    CommonModule,
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatChipsModule,
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    NgClass,
+    MatCardContent,
+    MatDivider,
+    MatCardActions,
+    FlexModule
+  ],
   templateUrl: './following.component.html',
   standalone: true,
   styleUrl: './following.component.css'
 })
 export class FollowingComponent {
-
+  patients = [
+    {
+      name: 'Jean Dupont',
+      status: 'Stable',
+      bloodPressure: '120/80',
+      temperature: 36.8,
+      heartRate: 75,
+      spo2: 98,
+      lastNote: 'Patient stable. Poursuite du traitement actuel.'
+    },
+  ];
 }
