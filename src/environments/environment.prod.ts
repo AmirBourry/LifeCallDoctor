cb387da8e051ad1e42f8bac0e84dde81d6539653
DamienStengel <3damyscus@gmail.com>
2025-01-14 14:07:53 +0100

    Firebase hosting

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBSL_M8GlsTaAhX0340f35Xu8FZapY1sgU",
+    authDomain: "cpcs-9cdae.firebaseapp.com",
+    projectId: "cpcs-9cdae",
+    storageBucket: "cpcs-9cdae.firebasestorage.app",
+    messagingSenderId: "856003321702",
+    appId: "1:856003321702:web:ac28f6b216f26e492c32a7",
+    measurementId: "G-QW4EELKMDN",
+    databaseURL: "https://cpcs-9cdae-default-rtdb.europe-west1.firebasedatabase.app"
+  }
+}; 
